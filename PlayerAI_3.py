ec1038375ddbd564ade07628a45b73218b26cb03
Abhishek Paudel <myheartswift@gmail.com>
2018-03-13 14:42:27 +0545

    new utility test, 2048 achieved

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -160,7 +160,7 @@ class PlayerAI(BaseAI):
 	# 	#print "heuristic",f1,f2,f4,f5,f6;
 	# 	sv= f1+f2+f4+f5+f6  # static value
 	# 	return sv
-
+# --------------------------------
 	def evaluate(self, grid):
 		# monotonicity = np.sum(self.mon_grad*np.array(grid.map))
 		# value = len(grid.getAvailableCells())**2 + grid.getMaxTile()**0.5 + monotonicity**0.5
@@ -238,4 +238,185 @@ class PlayerAI(BaseAI):
 			if grid.crossBound(cell) or not grid.canInsert(cell):
 				return {'far': prev, 'nxt': cell}
 
+# -------------------------------------------
 
+	def evaluate(self, board,commonRatio=0.25):
+		linearWeightedVal = 0
+		invert = False
+		weight = 1.
+		malus = 0
+		criticalTile = (-1,-1)
+		for y in range(0,4):
+			for x in range(0,4):
+				b_x = x
+				b_y = y
+				if invert:
+					b_x = 4 - 1 - x
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile == (-1,-1)):
+					criticalTile = (b_x,b_y)
+				linearWeightedVal += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		linearWeightedVal2 = 0
+		invert = False
+		weight = 1.
+		malus = 0
+		criticalTile2 = (-1,-1)
+		for x in range(0,4):
+			for y in range(0,4):
+				b_x = x
+				b_y = y
+				if invert:
+					b_y = 4 - 1 - y
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile2 == (-1,-1)):
+					criticalTile2 = (b_x,b_y)
+				linearWeightedVal2 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		
+		linearWeightedVal3 = 0
+		invert = False
+		weight = 1.
+		malus = 0
+		criticalTile3 = (-1,-1)
+		for y in range(0,4):
+			for x in range(0,4):
+				b_x = x
+				b_y = 4 - 1 - y
+				if invert:
+					b_x = 4 - 1 - x
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile3 == (-1,-1)):
+					criticalTile3 = (b_x,b_y)
+				linearWeightedVal3 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		linearWeightedVal4 = 0
+		invert = False
+		weight = 1.
+		malus = 0
+		criticalTile4 = (-1,-1)
+		for x in range(0,4):
+			for y in range(0,4):
+				b_x = 4 - 1 - x
+				b_y = y
+				if invert:
+					b_y = 4 - 1 - y
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile4 == (-1,-1)):
+					criticalTile4 = (b_x,b_y)
+				linearWeightedVal4 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+			
+		linearWeightedVal5 = 0
+		invert = True
+		weight = 1.
+		malus = 0
+		criticalTile5 = (-1,-1)
+		for y in range(0,4):
+			for x in range(0,4):
+				b_x = x
+				b_y = y
+				if invert:
+					b_x = 4 - 1 - x
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile5 == (-1,-1)):
+					criticalTile5 = (b_x,b_y)
+				linearWeightedVal5 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		linearWeightedVal6 = 0
+		invert = True
+		weight = 1.
+		malus = 0
+		criticalTile6 = (-1,-1)
+		for x in range(0,4):
+			for y in range(0,4):
+				b_x = x
+				b_y = y
+				if invert:
+					b_y = 4 - 1 - y
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile6 == (-1,-1)):
+					criticalTile6 = (b_x,b_y)
+				linearWeightedVal6 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		
+		linearWeightedVal7 = 0
+		invert = True
+		weight = 1.
+		malus = 0
+		criticalTile7 = (-1,-1)
+		for y in range(0,4):
+			for x in range(0,4):
+				b_x = x
+				b_y = 4 - 1 - y
+				if invert:
+					b_x = 4 - 1 - x
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile7 == (-1,-1)):
+					criticalTile7 = (b_x,b_y)
+				linearWeightedVal7 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		linearWeightedVal8 = 0
+		invert = True
+		weight = 1.
+		malus = 0
+		criticalTile8 = (-1,-1)
+		for x in range(0,4):
+			for y in range(0,4):
+				b_x = 4 - 1 - x
+				b_y = y
+				if invert:
+					b_y = 4 - 1 - y
+				#linearW
+				currVal=board.getCellValue((b_x,b_y))
+				if(currVal == 0 and criticalTile8 == (-1,-1)):
+					criticalTile8 = (b_x,b_y)
+				linearWeightedVal8 += currVal*weight
+				weight *= commonRatio
+			invert = not invert
+			
+		maxVal = max(linearWeightedVal,linearWeightedVal2,linearWeightedVal3,linearWeightedVal4,linearWeightedVal5,linearWeightedVal6,linearWeightedVal7,linearWeightedVal8)
+		if(linearWeightedVal2 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal2
+			criticalTile = criticalTile2
+		if(linearWeightedVal3 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal3
+			criticalTile = criticalTile3
+		if(linearWeightedVal4 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal4
+			criticalTile = criticalTile4
+		if(linearWeightedVal5 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal5
+			criticalTile = criticalTile5
+		if(linearWeightedVal6 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal6
+			criticalTile = criticalTile6
+		if(linearWeightedVal7 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal7
+			criticalTile = criticalTile7
+		if(linearWeightedVal8 > linearWeightedVal):
+			linearWeightedVal = linearWeightedVal8
+			criticalTile = criticalTile8
+		
+		return maxVal
